505c8ce5196c6732777a2025130e469526feeb90
qqqqqqq-bit <156386814+qqqqqqq-bit@users.noreply.github.com>
2025-06-06 11:30:06 +0800

    Update 5.qc_clean_fastq.sh

--- scripts/Exome upstream analysis/5.qc_clean_fastq.sh
+++ scripts/Exome upstream analysis/5.qc_clean_fastq.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 功能：对 clean_fq 中的 fastq.gz 文件进行 FastQC 质量控制并使用 MultiQC 汇总
+# Function: FastQC quality control of fastq.gz files in clean_fq and summarize using MultiQC
 
-# Conda 环境准备
+# Conda Environmental Preparation
 source ~/.bashrc
 conda activate nuggets
 
-# 设定路径
+# Set the path
 WORKDIR="/data/yuan/gastric_cancer"
 CLEAN_FASTQ_DIR="${WORKDIR}/2.clean_fq"
 QC_DIR="${WORKDIR}/3.qc/clean_qc"
@@ -14,16 +14,16 @@ CONFIG="${WORKDIR}/0.sra/config"
 
 mkdir -p ${QC_DIR}
 
-# 执行 FastQC
-echo "Step 8: 对 Clean FASTQ 文件进行 FastQC..."
+# Execute FastQC
+echo "Step 8: FastQC to Clean FASTQ files..."
 cd ${WORKDIR}
 cat ${CONFIG} | while read id; do
     fastqc --outdir ${QC_DIR} --threads 16 ${CLEAN_FASTQ_DIR}/${id}*.fq.gz \
     >> ${QC_DIR}/${id}_fastqc.log 2>&1
 done
 
-# MultiQC 汇总
-echo "生成 MultiQC 报告..."
+#MultiQC Summary
+echo "Generate MultiQC Reports..."
 multiqc ${QC_DIR}/*zip -o ${QC_DIR}/multiqc
 
-echo "FastQC & MultiQC 完成，结果保存在：${QC_DIR}"
+echo "FastQC & MultiQC Completed, the result is saved in：${QC_DIR}"
