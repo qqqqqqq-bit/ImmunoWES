8ee3bc52d718987b8c6f322d2e844596e0c68568
qqqqqqq-bit <156386814+qqqqqqq-bit@users.noreply.github.com>
2025-06-06 11:39:12 +0800

    Update 5.extract_mutation_position.py

--- scripts/HLA analysis/5.extract_mutation_position.py
+++ scripts/HLA analysis/5.extract_mutation_position.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import os
 
-# 输入目录路径
+# Enter the directory path
 input_dir = "/data/yuan/gastric_cancer/downstream_ukb/gastric_vcf/filter/filter_extract_csv"
 
-# 遍历输入目录中的所有 CSV 文件
+# Iterate through all CSV files in the input directory
 for filename in os.listdir(input_dir):
     if filename.endswith(".csv"):
         input_file = os.path.join(input_dir, filename)
@@ -13,7 +13,7 @@ for filename in os.listdir(input_dir):
             df = pd.read_csv(input_file, engine='python')
 
             if len(df.columns) < 12:
-                print(f"跳过文件 {filename}：列数不足12列")
+                print(f"Skip file {filename}: less than 12 columns")
                 continue
 
             mutation_col = df.columns[11]
@@ -26,7 +26,7 @@ for filename in os.listdir(input_dir):
             df[mutation_col] = df[mutation_col].apply(extract_last_mutation)
 
             df.to_csv(input_file, index=False)
-            print(f"处理并更新文件：{filename}")
+            print(f"Process and update files：{filename}")
 
         except Exception as e:
-            print(f"处理文件 {filename} 时发生错误: {e}")
+            print(f"An error occurred while processing file {filename}: {e}")
