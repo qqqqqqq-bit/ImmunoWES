56cbef4fcd3ac49d015cd73e9fa94b7e6f75d255
qqqqqqq-bit <156386814+qqqqqqq-bit@users.noreply.github.com>
2025-06-06 11:33:47 +0800

    Update 1.hla_typing.sh

--- scripts/HLA analysis/1.hla_typing.sh
+++ scripts/HLA analysis/1.hla_typing.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 
-# 功能：使用 HLA-HD 进行 HLA 等位基因分型（多线程并行）
+# Function: HLA allelic genotyping using HLA-HD (multi-threaded parallel)
 
-# Conda环境或系统路径可自定义
+# Conda environment or system path can be customized
 HLAHD_BIN="/data/yuan/wes_cancer/HLA-HD/hlahd.1.7.0/bin"
 FREQ_DATA="${HLAHD_BIN}/freq_data"
 GENE_LIST="${HLAHD_BIN}/HLA_gene.split.txt"
@@ -12,13 +12,13 @@ CONFIG="/data/yuan/gastric_cancer/config"
 CLEAN_FQ="/data/yuan/gastric_cancer/2.clean_fq"
 OUTDIR="/data/yuan/gastric_cancer/hla_estimation"
 
-# 拷贝 config 文件到工作目录中（可选）
+# Copy the config file to the working directory (optional)
 cp -r ${CONFIG} ${HLAHD_BIN}/
 
-# 切换到 HLA-HD 的 bin 目录
+# Switch to HLA-HD's bin directory
 cd ${HLAHD_BIN}
 
-# 使用 GNU Parallel 多线程执行每个样本的分型任务
+# Perform typing tasks for each sample using GNU Parallel multithreading
 cat config | parallel -j 16 --colsep '\t' './hlahd.sh -t 2 -m 100 -c 0.95 -f ${FREQ_DATA} \
     ${CLEAN_FQ}/{}_1_val_1.fq.gz \
     ${CLEAN_FQ}/{}_2_val_2.fq.gz \
