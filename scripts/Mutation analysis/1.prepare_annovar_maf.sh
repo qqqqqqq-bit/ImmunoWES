6135401be0519c94d0c6c05ac5d5b9390101babd
qqqqqqq-bit <156386814+qqqqqqq-bit@users.noreply.github.com>
2025-06-06 14:16:32 +0800

    Update 1.prepare_annovar_maf.sh

--- scripts/Mutation analysis/1.prepare_annovar_maf.sh
+++ scripts/Mutation analysis/1.prepare_annovar_maf.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd /data/yuan/gastric_cancer/7.annote/annovar1
 
-# 加样本名，截取前20列
+# Add sample name and intercept the first 20 columns
 cat config | while read id; do
     grep -v 'Chr' ${id}.hg38_multianno.txt | cut -f 1-20 | \
     awk -v sample_id="${id}" '{print $0 "\t" sample_id}' > ${id}.maf
 done
 
-# 添加表头
+# Add a header
 head -1 YSU_T9.hg38_multianno.txt | cut -f 1-20 | \
 awk '{print $0"\tTumor_Sample_Barcode"}' > header
 
-# 合并所有 MAF 文件
+# Merge all MAF files
 cat header *maf > all_sample.maf
